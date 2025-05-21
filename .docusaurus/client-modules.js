696f6f476facb8670f5c423185e14229cff8f989
Panák Zsófia <panakzsofiapublic@gmail.com>
2025-05-21 12:46:36 +0200

    Initial Docusaurus project setup

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/galszabolcs/Work/Docusaurus/zsofi-docu-site/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/galszabolcs/Work/Docusaurus/zsofi-docu-site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/galszabolcs/Work/Docusaurus/zsofi-docu-site/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/galszabolcs/Work/Docusaurus/zsofi-docu-site/src/css/custom.css"),
+];
